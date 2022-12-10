71e2dc3e1c83563c623d6cd3af5a9fedbd29f8c7
77zzl <pennyworth18@outlook.com>
2022-12-10 10:49:40 +0800

    add game app

--- game/views.py
+++ game/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    str = '<h1 style="text-align: center">Index Page</h1>'
+    return HttpResponse(str)
+
+def play(request):
+    str = '<h1 style="text-align: center">Game Page</h1>'
+    return HttpResponse(str)

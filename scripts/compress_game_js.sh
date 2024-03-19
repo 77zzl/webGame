f4f36de1387898b8e99cc2200cddb5cedfae11d9
77zzl <pennyworth18@outlook.com>
2024-03-19 17:45:52 +0800

    update js format

--- scripts/compress_game_js.sh
+++ scripts/compress_game_js.sh
@@ -5,6 +5,9 @@ JS_PATH=/home/developer/webGame/game/static/js/
 JS_PATH_DIST=${JS_PATH}dist
 JS_PATH_SRC=${JS_PATH}src
 
+find $JS_PATH_SRC -type f -name '*.js' | sort | terser -c -m > ${JS_PATH_DIST}/game.js
+
+# 以下注释将删除换行符
 # find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat | terser -c -m > ${JS_PATH_DIST}/game.js
 
 echo yes | python3 ${BASE}manage.py collectstatic
